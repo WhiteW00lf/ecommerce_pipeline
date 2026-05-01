a3d14c7b0f5b87ae09d8abf508e0e734d85d75d5
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-05-01 13:54:10 +0530

    added business queries

---
+++ ecommercepipeline/models/marts/daily_revenue.sql
@@ -0,0 +1,10 @@
+SELECT 
+    order_date,
+    SUM(total_order_value) AS daily_revenue,
+    count(DISTINCT order_id) AS daily_orders,
+    COUNT(DISTINCT customer_id) AS unique_customers
+
+FROM {{ ref('fct_orders') }}
+WHERE status != 'cancelled'
+GROUP BY order_date 
+ORDER BY order_date ASC;
---
+++ ecommercepipeline/models/marts/fct_orders.sql
@@ -0,0 +1,18 @@
+SELECT 
+    o.order_id,
+    o.customer_id,
+    c.first_name,
+    c.last_name,
+    o.product_id,
+    p.product_name,
+    o.quantity,
+    o.price AS order_price,
+    p.price AS product_price,
+    o.order_date,
+    o.delivery_date,
+    o.status,
+    o.quantity * o.price AS total_order_value
+
+FROM {{ ref('stg_orders') }} AS o
+JOIN {{ ref('stg_customer') }} AS c ON o.customer_id = c.customer_id
+JOIN {{ ref('stg_products') }} AS p ON o.product_id = p.product_id
